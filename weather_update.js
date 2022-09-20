219eafdcc8abcbfa9824c7358d47439fec72a383
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 <sharath.yaragatti@yahoo.com>
2022-09-20 13:53:24 +0530

    ""

--- weather_update.js
+++ weather_update.js
@@ -1,15 +1,22 @@
 let geoLocation=()=>{
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(show);
+        navigator.geolocation.getCurrentPosition(setPosition,showError);
     }
     else{
-        document.getElementById('temperature')="There is no geolocation support for your browser";
-        document.getElementById('weather-type')="";
+        document.getElementById('temperature')="";
+        document.getElementById('type')="";
+        document.getElementById('notification').style.display="block";
+        document.getElementById('notification').innerHTML="There is no geolocation support for your browser";
     }
 }
 
-let show=(data)=>{
-console.log(data);
+let setPosition=(data)=>{
+let latitude = data.coords.latitude;
+let longitude= data.coords.longitude; 
+getWeather(latitude,longitude);
+}
+let showError=(err)=>{
+    document.getElementById('notification').innerHTML=`<span>${err.message}</span>`;
 }
 let data=navigator.geolocation.getCurrentPosition(show).coords;
 let lat = data.coords.latitude;
