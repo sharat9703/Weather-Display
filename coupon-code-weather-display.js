01037ff338c6e801225f8cd1ef70857b44aff31b
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 <sharath.yaragatti@yahoo.com>
2022-09-27 21:51:38 +0530

    formatted with prettier

--- coupon-code-weather-display.js
+++ coupon-code-weather-display.js
@@ -20,61 +20,61 @@ coupon.onclick = () => {
     alert("copied to clipboard");
   });
 };
-let geoLocation=()=>{
-  if(navigator.geolocation){
+let geoLocation = () => {
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(show);
   }
-}
-let show=(data)=>{
+};
+let show = (data) => {
   console.log(data);
   let latitude = data.coords.latitude;
-  let longitude= data.coords.longitude; 
+  let longitude = data.coords.longitude;
   console.log(`latitude is ${latitude} longitude is ${longitude}`);
-  getWeather(latitude,longitude);
-}
-const key ="fbf712a5a83d7305c3cda4ca8fe7ef29";
-let getWeather=(latitude,longitude)=>{
-  let api =`http://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=5&appid=${key}`;
-console.log(api);
+  getWeather(latitude, longitude);
+};
+const key = "fbf712a5a83d7305c3cda4ca8fe7ef29";
+let getWeather = (latitude, longitude) => {
+  let api = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=5&appid=${key}`;
+  console.log(api);
   fetch(api)
-  .then((response)=>{
-let data= response.json();
-return data;
-  })
-  .then((data)=>{
-    let today =new Date();
-    weather.iconId = data.list[0].weather[0].icon;
-    if(today.getHours()>6 && today.getHours()<18){
-      weather.temperature =  Math.ceil(data.list[0].temp.day);
-    }
-    else{
-      weather.temperature =  Math.ceil(data.list[0].temp.night);
-      weather.iconId = weather.iconId.split("").slice(0,2).concat("n").join("");
-    }
-    
-    weather.city = data.city.name;
-    weather.description = data.list[0].weather[0].description;
-   
-  
-  }).then(()=>{
-    displayWeather();
-    console.log(weather.iconId);
-  });
-  
-}
-let weatherIcon=document.getElementById("w-icon");
+    .then((response) => {
+      let data = response.json();
+      return data;
+    })
+    .then((data) => {
+      let today = new Date();
+      weather.iconId = data.list[0].weather[0].icon;
+      if (today.getHours() > 6 && today.getHours() < 18) {
+        weather.temperature = Math.ceil(data.list[0].temp.day);
+      } else {
+        weather.temperature = Math.ceil(data.list[0].temp.night);
+        weather.iconId = weather.iconId
+          .split("")
+          .slice(0, 2)
+          .concat("n")
+          .join("");
+      }
+
+      weather.city = data.city.name;
+      weather.description = data.list[0].weather[0].description;
+    })
+    .then(() => {
+      displayWeather();
+      console.log(weather);
+    });
+};
+let weatherIcon = document.getElementById("w-icon");
 let place = document.getElementById("place");
 let temperature = document.getElementById("temperature");
-let weatherType= document.getElementById("type");
+let weatherType = document.getElementById("type");
 let notification = document.getElementById("notification");
-function displayWeather(){
-weatherIcon.innerHTML =`<img src="./icons/${weather.iconId}.png"
+function displayWeather() {
+  weatherIcon.innerHTML = `<img src="./icons/${weather.iconId}.png"
  style="height:34px;"/>`;
-temperature.innerHTML = `${weather.temperature}°<span> C,</span>`;
-place.innerHTML=`${weather.city},`;
-weatherType.innerHTML=`${weather.description}`; 
+  temperature.innerHTML = `${weather.temperature}°<span> C,</span>`;
+  place.innerHTML = `${weather.city},`;
+  weatherType.innerHTML = `${weather.description}`;
 }
-
 
 window.onload = () => {
   geoLocation();
@@ -84,12 +84,9 @@ window.onload = () => {
 
 let close_button = document.querySelector(".close");
 
-
 //////////////////////////////weather-API part////////////////////////////
-let weather ={};
+let weather = {};
 
 close_button.onclick = () => {
   coupon.classList.toggle("d-none");
-  
-  
 };
